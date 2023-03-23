cb3bf24bfe71bd95286c58dcbaed075b8e4d7fff
christopher-caldwell <well.done.514@gmail.com>
2023-03-23 13:31:34 -0500

    fix: Example types

--- example/index.ts
+++ example/index.ts
@@ -2,9 +2,9 @@ import { bulkWebPConvert } from '@caldwell619/bulk-webp-converter'
 
 // relative from execution - this is called from the root
 const pathToSource = 'example/source-images'
-const pathToOut = 'example/out'
+const pathToOutput = 'example/out'
 
-bulkWebPConvert(pathToSource, pathToOut, 100, 1)
+bulkWebPConvert({ pathToSource, pathToOutput, quality: 100, parallelLimit: 1 })
   .then(() => {
     process.exit()
   })
