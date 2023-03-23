fc9ae181b93ddfa722ea7425d2ee7b91d2946e53
christopher-caldwell <well.done.514@gmail.com>
2023-03-23 10:54:35 -0500

    feat: Updating tooling

--- webp-converter.d.ts
+++ webp-converter.d.ts
@@ -1,21 +1,12 @@
 declare module 'webp-converter' {
   const grant_permission: () => void
   /** Convert other image format to webp */
-  const cwebp: (
-    src: string,
-    outputFileName: string,
-    quality: string,
-    logging?: string,
-  ) => Promise<string>
+  const cwebp: (src: string, outputFileName: string, quality: string, logging?: string) => Promise<string>
   /**
    * @argument extension {string} "jpg", "png"
    * @argument quality {string} "-q 80"
    */
-  const str2webpstr: (
-    dataBase64: string,
-    extension: string,
-    quality: string,
-  ) => Promise<unknown>
+  const str2webpstr: (dataBase64: string, extension: string, quality: string) => Promise<unknown>
   const buffer2webpbuffer: () => Promise<unknown>
   const dwebp: () => Promise<unknown>
   /** Convert gif image to webp */
